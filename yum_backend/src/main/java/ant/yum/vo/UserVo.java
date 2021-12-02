1b92de18985640ce5327913c1c16171610221a01
jooeui <kje_0727@naver.com>
2021-12-02 20:04:43 +0900

    211202 / 주의 / create / backend 와꾸!

---
+++ yum_backend/src/main/java/ant/yum/vo/UserVo.java
@@ -0,0 +1,20 @@
+package ant.yum.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserVo {
+	
+	private int uNo;
+	private String uPassword;
+	private String Uname;
+	private String uRRN;
+	private String uJob;
+	private String uAuth;
+	private String uPhone;
+	private String uGender;
+}
