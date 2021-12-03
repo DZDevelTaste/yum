b7f0139da450ba4c4518d660b37917f1a585eab7
hanjoohyung <ghd167@naver.com>
2021-12-03 17:06:13 +0900

    211203 / 주형 / update / schedule service repository 수정

--- yum_backend/src/main/java/ant/yum/repository/ScheduleRepository.java
+++ yum_backend/src/main/java/ant/yum/repository/ScheduleRepository.java
@@ -2,9 +2,13 @@ package ant.yum.repository;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import ant.yum.vo.ScheduleVo;
 
 @Repository
 public class ScheduleRepository {
@@ -13,6 +17,20 @@ public class ScheduleRepository {
 
 	public List<Object> findBySchedule() {
 		return sqlSession.selectList("schedule.findBySchedule");
+	}
+
+	public boolean updateSchedule(int userNo) {
+		int count = sqlSession.update("schedule.updateSchedule", userNo);
+		return count == 1;
+	}
+
+	public boolean deleteSchedule(int userNo) {
+		int count = sqlSession.delete("schedule.deleteSchedule", userNo);
+		return count == 1;
+	}
+
+	public boolean addSchedule(@Valid ScheduleVo vo) {
+		return sqlSession.selectOne("disease.addDisease", vo);
 	}
 
 }
--- yum_backend/src/main/java/ant/yum/service/ScheduleService.java
+++ yum_backend/src/main/java/ant/yum/service/ScheduleService.java
@@ -2,10 +2,13 @@ package ant.yum.service;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ant.yum.repository.ScheduleRepository;
+import ant.yum.vo.ScheduleVo;
 
 @Service
 public class ScheduleService {
@@ -15,5 +18,14 @@ public class ScheduleService {
 	
 	public List<Object> findByAll() {
 		return scheduleRepository.findBySchedule();
+	}
+	public boolean deleteSchedule(int userNo) {
+		return scheduleRepository.deleteSchedule(userNo);
+	}
+	public boolean addSchedule(@Valid ScheduleVo vo) {
+		return scheduleRepository.addSchedule(vo);
+	}
+	public void updateSchedule(int userNo) {
+		scheduleRepository.updateSchedule(userNo);
 	}
 }
