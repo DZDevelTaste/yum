4db3f2e38157236a8422e9cedcd0fb00456af368
hanjoohyung <ghd167@naver.com>
2021-12-18 14:00:16 +0900

    211218 / 주형 / update

--- yum_frontend/src/layout/Navigation.js
+++ yum_frontend/src/layout/Navigation.js
@@ -17,9 +17,6 @@ const navigation = () => {
         : job == 'D' ? 
         <nav className={styles.Main}>
             <NavLink to={'/doctor'} end>메인</NavLink>
-            <NavLink to={'/nurse/patients'}>환자리스트</NavLink>
-            <NavLink to={'/nurse/order'}>접수</NavLink>
-            <NavLink to={'/nurse/reservation'}>예약관리</NavLink>
             <NavLink to={'/schedule'}>스케쥴러</NavLink>
         </nav> : 
         <nav className={styles.Main}>
