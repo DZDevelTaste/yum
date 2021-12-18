8d353ec3c3b7ba44ed8e62d6c01478c5916dab1e
홍창목 <hong1071@nate.com>
2021-12-18 14:06:14 +0900

    211218 / 창목 / create / 레이아웃

---
+++ yum_frontend/src/assets/scss/layout/SiteLayout.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Navigation from "./Navigation";
+import styles from '../assets/scss/Content.scss'
+
+export default function SiteLayout({children}) {
+    return (
+        <Fragment>
+            <Navigation/>
+            <div className={styles.Content}>
+                {children}
+            </div>
+        </Fragment>
+    );
+}
