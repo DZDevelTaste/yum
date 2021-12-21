a906a4e273ff52adcb92ea241658af6876a7dea9
jooeui <kje_0727@naver.com>
2021-12-21 17:37:41 +0900

    211221 / 주의 / update /

--- yum_frontend/src/layout/Navigation.js
+++ yum_frontend/src/layout/Navigation.js
@@ -3,14 +3,10 @@ import { NavLink } from 'react-router-dom';
 import styles from '../assets/scss/layout/Navigation.scss';
 
 const navigation = () => {
-<<<<<<< HEAD
-    return (
-=======
     const job = sessionStorage.getItem("job");
     return (
         <div>
         { job == 'N' ?
->>>>>>> origin/master
         <nav className={styles.Main}>
             <NavLink to={'/nurse'} end>메인</NavLink>
             <NavLink to={'/nurse/patients'}>환자리스트</NavLink>
@@ -18,8 +14,6 @@ const navigation = () => {
             <NavLink to={'/nurse/reservation'}>예약관리</NavLink>
             <NavLink to={'/schedule'}>스케쥴러</NavLink>
         </nav>
-<<<<<<< HEAD
-=======
         : job == 'D' ? 
         <nav className={styles.Main}>
             <NavLink to={'/doctor'} end>메인</NavLink>
@@ -33,7 +27,6 @@ const navigation = () => {
         </nav>
         }
         </div>
->>>>>>> origin/master
     );
 };
 
